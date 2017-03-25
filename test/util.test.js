ca67ff48df5d8da0f03bb9ee5763f3a2488f9d0b
Jake Rowsell <jake@qubit.com>
2017-03-25 21:33:01 +0000

    Both methods in lib/util are now tested

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-const { sortFrequenciesByWeight } = require('../lib/util')
+const { sortFrequenciesByWeight, convertFrequenciesToArray } = require('../lib/util')
 const node1 = {
   characters: 'a',
   weight: 15
@@ -15,4 +15,28 @@ const node3 = {
 it('Sorts by weight property ascending', () => {
   const nodes = [node3, node1, node2]
   expect(sortFrequenciesByWeight(nodes).map(n => n.characters).join('')).toBe('abc')
+})
+
+it('Can convert a frequencies object to an array of frequency nodes', () => {
+  const frequencyObject = {
+    'a': 15,
+    'b': 22,
+    'c': 1102
+  }
+  const resultArray = convertFrequenciesToArray(frequencyObject)
+
+  expect(resultArray).toEqual([
+    {
+      characters: 'a',
+      weight: 15
+    },
+    {
+      characters: 'b',
+      weight: 22
+    },
+    {
+      characters: 'c',
+      weight: 1102
+    }
+  ])
 })
